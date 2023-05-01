c225737f8ebc33cbb4daef70b830821a9c7957b4
Sara Orsi <sararaorsi@gmail.com>
2023-05-01 19:40:34 +0100

    loading

--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -24,81 +24,84 @@ export function Country() {
             });
     }, [code]);
 
+    if (isLoading) {
+        return <div>Loading........</div>
+    } else if (error) {
+        return { error }
+    } else {
+        return (
+            <div className={styles.container}>
+                {country?.map((country, index) => (
+                    <div className={styles.country} key={index}>
+                        <div>
+                            <img className={styles.flag} src={country.flags.png} alt={country.name.common} />
+                        </div>
+                        <div>
+                            <section className={styles.header}>
+                                <div className={styles.name}>
+                                    {country.name.common}
+                                </div>
+                                <div className={styles.capital}>
+                                    {country.capital}
+                                </div>
+                            </section>
 
-    return (
+                            <section>
+                                <div className={styles.info}>
+                                    <div>
+                                        Region
+                                    </div>
+                                    <div>
+                                        {country.region}
+                                    </div>
+                                </div>
+                                <div className={styles.info}>
+                                    <div>
+                                        Subregion
+                                    </div>
+                                    <div>
+                                        {country.subregion}
+                                    </div>
+                                </div>
+                                <div className={styles.info}>
+                                    <div>
+                                        Population
+                                    </div>
+                                    <div>
+                                        {new Intl.NumberFormat().format(country.population)}
+                                    </div>
+                                </div>
+                                <div className={styles.info}>
+                                    <div>
+                                        Languages
+                                    </div>
+                                    <div>
+                                        {country.languages && Object.values(country.languages).map(language => language).join(', ')}
+                                    </div>
+                                </div>
+                                <div className={styles.info}>
+                                    <div>
+                                        Currencies
+                                    </div>
+                                    <div>
+                                        {country.currencies && Object.values(country.currencies).map(currency => currency.name).join(', ')}
+                                    </div>
+                                </div>
+                                {country.maps.googleMaps &&
+                                    <a className={styles.map} href={country.maps.googleMaps} target="_blank">
+                                        Find on Map
+                                    </a>
+                                }
+                            </section>
 
-        <div className={styles.container}>
-            {isLoading && !error && <div>Loading........</div>}
-            {error && !isLoading && { error }}
-            {country?.map((country, index) => (
-                <div className={styles.country} key={index}>
-                    <div>
-                        <img className={styles.flag} src={country.flags.png} alt={country.name.common} />
+                        </div>
                     </div>
-                    <div>
-                        <section className={styles.header}>
-                            <div className={styles.name}>
-                                {country.name.common}
-                            </div>
-                            <div className={styles.capital}>
-                                {country.capital}
-                            </div>
-                        </section>
 
-                        <section>
-                            <div className={styles.info}>
-                                <div>
-                                    Region
-                                </div>
-                                <div>
-                                    {country.region}
-                                </div>
-                            </div>
-                            <div className={styles.info}>
-                                <div>
-                                    Subregion
-                                </div>
-                                <div>
-                                    {country.subregion}
-                                </div>
-                            </div>
-                            <div className={styles.info}>
-                                <div>
-                                    Population
-                                </div>
-                                <div>
-                                    {new Intl.NumberFormat().format(country.population)}
-                                </div>
-                            </div>
-                            <div className={styles.info}>
-                                <div>
-                                    Languages
-                                </div>
-                                <div>
-                                    {country.languages && Object.values(country.languages).map(language => language).join(', ')}
-                                </div>
-                            </div>
-                            <div className={styles.info}>
-                                <div>
-                                    Currencies
-                                </div>
-                                <div>
-                                    {country.currencies && Object.values(country.currencies).map(currency => currency.name).join(', ')}
-                                </div>
-                            </div>
-                            {country.maps.googleMaps &&
-                                <a className={styles.map} href={country.maps.googleMaps} target="_blank">
-                                    Find on Map
-                                </a>
-                            }
-                        </section>
+                ))
+                }
+            </div >
 
-                    </div>
-                </div>
 
-            ))
-            }
-        </div >
-
-    );
+        )
+    };
 }
